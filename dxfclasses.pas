1dc6c8d565098668262f712fdffd1f06a3e85214
Dmitry Boyarintsev <skalogryz.lists@gmail.com>
2020-12-15 10:27:34 -0500

    making layout a sub class of plot settings

--- dxfclasses.pas
+++ dxfclasses.pas
@@ -1114,14 +1114,101 @@ type
   TDxfAcDbPlaceHolder = class(TDxfObject)
   end;
 
+
+  { TDxfPlotSettings }
+
+  TDxfPlotSettings = class(TDxfObject)
+    SubClass2      : string;  // Subclass marker (AcDbPlotSettings)
+    PageName       : string;  //   1 Page Setup name
+    PrinterName    : string;  //   2 Name of system printer or plot configuration file
+    PaperSize      : string;  //   4 Paper size
+    PlotViewName   : string;  //   6 Plot view name
+    MarginLeftMm   : double;  //  40 Size, in millimeters, of unprintable margin on left side of paper
+    MarginBottomMm : double;  //  41 Size, in millimeters, of unprintable margin on bottom of paper
+    MarginRightMm  : double;  //  42 Size, in millimeters, of unprintable margin on right side of paper
+    MarginTopMm    : double;  //  43 Size, in millimeters, of unprintable margin on top of paper
+    WidthMm        : double;  //  44 Plot paper size: physical paper width in millimeters
+    HeightMm       : double;  //  45 Plot paper size: physical paper height in millimeters
+    OffsetX        : double;  //  46 Plot origin: X value of origin offset in millimeters
+    OffsetY        : double;  //  47 Plot origin: Y value of origin offset in millimeters
+    PlotX          : double;  //  48 Plot window area: X value of lower-left window corner
+    PlotY          : double;  //  49 Plot window area: Y value of upper-right window corner
+    PlotX2         : double;  // 140 Plot window area: X value of lower-left window corner
+    PlotY2         : double;  // 141 Plot window area: Y value of upper-right window corner
+    Numerator      : double;  // 142 Numerator of custom print scale: real world (paper) units
+    Denominator    : double;  // 143 Denominator of custom print scale: drawing units
+    Flags          : integer; //  70 Plot layout flag:
+                              //     1 = PlotViewportBorders
+                              //     2 = ShowPlotStyles
+                              //     4 = PlotCentered
+                              //     8 = PlotHidden
+                              //     16 = UseStandardScale
+                              //     32 = PlotPlotStyles
+                              //     64 = ScaleLineweights
+                              //     128 = PrintLineweights
+                              //     512 = DrawViewportsFirst
+                              //     1024 = ModelType
+                              //     2048 = UpdatePaper
+                              //     4096 = ZoomToPaperOnUpdate
+                              //     8192 = Initializing
+                              //     16384 = PrevPlotInit
+    PlotUnits      : integer; //  72 Plot paper units:
+                              //     0 = Plot in inches
+                              //     1 = Plot in millimeters
+                              //     2 = Plot in pixels
+    PlotRotation   : integer; //  73 Plot rotation:
+                              //     0 = No rotation
+                              //     1 = 90 degrees counterclockwise
+                              //     2 = Upside-down
+                              //     3 = 90 degrees clockwise
+    PlotType       : integer; //  74 Plot type (portion of paper space to output to the media):
+                              //     0 = Last screen display
+                              //     1 = Drawing extents
+                              //     2 = Drawing limits
+                              //     3 = View specified by code 6
+                              //     4 = Window specified by codes 48, 49, 140, and 141
+                              //     5 = Layout information
+                              //     7 Current style sheet
+    StdScale       : integer; //  75 Standard scale type:
+                              //     0 = Scaled to Fit
+                              //     1 = 1/128"=1'; 2 = 1/64"=1'; 3 = 1/32"=1'
+                              //     4 = 1/16"=1'; 5 = 3/32"=1'; 6 = 1/8"=1'
+                              //     7 = 3/16"=1'; 8 = 1/4"=1'; 9 = 3/8"=1'
+                              //     10 = 1/2"=1'; 11 = 3/4"=1'; 12 = 1"=1'
+                              //     13 = 3"=1'; 14 = 6"=1'; 15 = 1'=1'
+                              //     16 = 1:1 ; 17= 1:2; 18 = 1:4; 19 = 1:8; 20 = 1:10; 21= 1:16
+                              //     22 = 1:20; 23 = 1:30; 24 = 1:40; 25 = 1:50; 26 = 1:100
+                              //     27 = 2:1; 28 = 4:1; 29 = 8:1; 30 = 10:1; 31 = 100:1; 32 = 1000:1
+    ShadePlotMode  : integer; //  76 ShadePlot mode:
+                              //     0 = As Displayed
+                              //     1 = Wireframe
+                              //     2 = Hidden
+                              //     3 = Rendered
+    ShadePlotRes   : integer; //  77 ShadePlot resolution level:
+                              //     0 = Draft
+                              //     1 = Preview
+                              //     2 = Normal
+                              //     3 = Presentation
+                              //     4 = Maximum
+                              //     5 = Custom
+    ShadePlotDPI   : integer; //  78 ShadePlot custom DPI:
+                              //     Valid range: 100 to 32767
+                              //     Only applied when the ShadePlot resolution level is set to 5 (Custom)
+    ViewScale      : double;  // 147 A floating point scale factor that represents the standard scale value
+                              //     specified in code 75
+    PaperImgOrigX  : double;  // 148 Paper image origin: X value
+    PaperImgOrigY  : double;  // 149 Paper image origin: Y value
+    ShadePlotID    : string;  // 333 ShadePlot ID/Handle (optional)
+  end;
+
   { TDxfLayout }
   //
   //LAYOUT
   //
-  TDxfLayout = class(TDxfObject)
-    SubClass2  : string;      // 100 Subclass marker (DictionaryVariables)
+  TDxfLayout = class(TDxfPlotSettings)
+    SubClass3  : string;      // 100 Subclass marker (DictionaryVariables)
     LayoutName : string;      //   1 Layout name
-    Flags      : Integer;     //  70 Flag (bit-coded) to control the following:
+    LayoutFlags: Integer;     //  70 Flag (bit-coded) to control the following:
                               //     1 = Indicates the PSLTSCALE value for this layout when this layout is current
                               //     2 = Indicates the LIMCHECK value for this layout when this layout is current
     TabOrder   : Integer;     //  71 Tab order. This number is an ordinal indicating this layout's ordering in the tab control that is
@@ -1146,7 +1233,7 @@ type
     UcsId       : string;     // 345 ID/handle of AcDbUCSTableRecord if UCS is a named UCS. If not present, then UCS is unnamed
     UcsOrthoId  : string;     // 346 ID/handle of AcDbUCSTableRecord of base UCS if UCS is orthographic (76 code is non-zero).
                               //     If not present and 76 code is non-zero, then base UCS is taken to be WORLD
-    ShadePlotId : string;     // 333 Shade plot ID
+    ShadePlotId2: string;     // 333 Shade plot ID
   end;
 
   { TDxfDictionaryVar }
